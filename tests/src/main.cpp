1e59b5d7003bb62c5ff8b5c4187bd727a118ddf5
ben-creates <kweon_sunbeom@student.smc.edu>
2021-01-12 03:47:38 +0900

    CMake (1)

---
+++ tests/src/main.cpp
@@ -0,0 +1,58 @@
+/* Every header files working in progress here */
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+//#include "../include/public_header.hpp"
+#include "stock.cpp"
+#include "csvExtractor.cpp"
+
+
+
+int main(){
+
+    srand(time(NULL));
+    vector<vector<string>> v;
+    vector<int> vv;
+
+    CSVExtractor* c = new CSVExtractor("../src/companies.csv");
+    RandomNumberGenerator* n = new RandomNumberGenerator(1, 10, 3);
+
+    v = c->GetResult();
+    vv = n->GetNumbers();
+
+    // CSVExtractor
+    for(std::size_t i = 0; i < v.size(); i++){
+        for(int j = 0; j < 3; j++){
+            switch (j) {
+                case 0: cout << "Symbol:\t"; break;
+                case 1: cout << "Name:\t"; break;
+                case 2: cout << "Type:\t"; break;
+            }
+             cout << v.at(i).at(j) << endl;
+        }
+    }
+
+    // GenerateRandomNumbers
+    for(std::size_t i = 0; i < vv.size(); i++)
+        cout << vv.at(i) << endl;
+
+    Stock* s  = new Stock("Ben's ");
+    cout << s->GetChangedPercentage() << endl; // => 0%
+    s->SetCurrentPrice(214.00);
+    s->UpdateStockPrice();
+    cout << s->GetChangedPercentage() << endl;
+
+    delete s;
+    s = NULL;
+
+    delete c;
+    c = NULL;
+
+    delete n;
+    n = NULL;
+
+    return 0;
+}
---
+++ tests/src/stock.hpp
@@ -0,0 +1,81 @@
+#ifndef STOCK_H
+#define STOCK_H
+
+#include <time.h>
+#include <ctime>
+#include <vector>
+
+enum TRANSACTION_TYPE { SELL, BUY };
+
+struct Transaction{
+    Transaction(int, double);
+    string currentTime;
+    int type;
+    double cost;
+
+    // ToString
+    friend ostream& operator<<(ostream& strm, const Transaction& t);
+};
+
+struct Company{
+    Company(string);
+    Company(string, string);
+    string companyName;
+    string companyType;
+    double companyPrice;
+    double companyValue;
+
+    // ToString
+    friend ostream& operator<<(ostream& strm, const Company& c);
+};
+
+class Stock{
+public:
+    Stock(string);
+    Stock(string, double, Company*);
+
+    // Get
+    double GetCurrentPrice();
+    double GetPrevPrice();
+    double GetChangedPercentage();
+    string GetSymbol();
+    Company GetStockIssuer();
+    vector<Transaction> GetTransactionHistory();
+
+    // Set
+    void SetCurrentPrice(double);
+    void SetPrevPrice(double);
+    void SetSymbol(string);
+    void SetStockIssuer(Company*); // Just passing pointer
+
+    // Methods
+    void AddTransactionHistory(int, double);
+    void UpdateChangedPercentage();
+    void UpdateStockPrice();
+    int GetRandomNumber(int);
+    double GetRandomStockPrice(int = 4);
+
+    // In Progress
+
+
+    // Tests
+    void PrintTransactionHistory();
+
+    // Dectructor
+    ~Stock();
+
+protected:
+    double stockCurrentPrice;
+    double stockPrevPrice;
+    double stockUniqueConstant = 0.01418953;
+    double stockChangedPercentage;
+    string stockSymbol;
+    Company* stockIssuer; // Stock Issuer
+    vector<Transaction> transactionHistory; // Neead fix
+
+
+    // ToString
+    friend ostream& operator<<(ostream& strm, const Stock& s);
+};
+
+#endif
