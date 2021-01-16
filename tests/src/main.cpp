81f5be5fe10e2278ab53ef6671f20cda16155f14
ben-creates <kweon_sunbeom@student.smc.edu>
2021-01-17 08:27:40 +0900

    Organazie files

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -3,35 +3,50 @@
 #include "stock.cpp"
 #include "csvExtractor.cpp"
 #include "banking.cpp"
+#include "player.cpp"
 
 int main(){
 
     /* Initializing Data */
-
     srand(time(NULL));
     string username;
-    vector<vector<string>> v;
-    vector<int> vv;
+    vector<vector<string>> data;
+    vector<int> index;
     CSVExtractor* c = new CSVExtractor("companies.csv");
     RandomNumberGenerator* n = new RandomNumberGenerator(1, 40, 15);
-    // Generate stocks
+
+    // Generate bank Account
     cout << "Input user name\n" << endl;
     cin >> username;
     Account* a = new Account(username, 9);
     a->info_Account(*a);
     cout << "Hello World" << endl;
 
-    v = c->GetResult();
-    vv = n->GetNumbers();
-    cout << v.at(2).at(1) << endl;
+    // Generate stocks
+    // GenerateStocks();
+
+    data = c->GetResult();
+    index = n->GetNumbers();
 
     delete c;
     delete n;
-    delete a;
-
     c=NULL;
     n=NULL;
+
+    /* End of the game */
+    delete a;
     a=NULL;
 
+    system("pause");
     return 0;
+}
+
+/* Initializers */
+vector<Company*> GenerateCompanies(){
+    vector<Company*> result;
+    return result;
+}
+vector<Stock*> GenerateStocks(){
+    vector<Stock*> result;
+    return result;
 }
