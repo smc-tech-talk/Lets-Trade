cd8e471023c5e481801525d61774826847a65f26
ben-creates <kweon_sunbeom@student.smc.edu>
2021-01-18 16:37:33 +0900

    Transaction::Transaction()

--- tests/src/transaction.cpp
+++ tests/src/transaction.cpp
@@ -1,40 +1,33 @@
 // Followed the charyt in https://www.interactivebrokers.com/en/software/am3/am/funding/viewingtransactionhistory.htm
 // Date | Status | Type | Amount/Postion | Status
-#ifndef TRANSACTION_H
-#define TRANSACTION_H
 
 #include "transaction.hpp"
 
 time_t NOW = time(0); // current system date/time
 tm* GMT_TIME = gmtime(&NOW);
+
 /* Date */
-Date::Date(int year, int month, int day, int hour, string real_time){
-    this->year = year;
-    this->month = month;
-    this->day = day;
-    this->hour = hour;
-    this->real_time = real_time;
-};
+Date::Date(){}; // Default Constructor
+Date::Date(int year, int month, int day, int hour, string real_time)
+    :year(year),
+    month(month),
+    day(day),
+    hour(hour),
+    real_time(real_time){};
 
 /* Transaction */
 // Test
-Transaction::Transaction(){
-    Date date = Date(
-        (1900 + (GMT_TIME->tm_year)),
-        (GMT_TIME->tm_mon),
-        (GMT_TIME->tm_mday),
-        (GMT_TIME->tm_hour),
-        ctime(&NOW)
-    );
+Transaction::Transaction() {
+    Date date;
+    date = Date(
+            (1900 + (GMT_TIME->tm_year)),
+            (GMT_TIME->tm_mon),
+            (GMT_TIME->tm_mday),
+            (GMT_TIME->tm_hour),
+            ctime(&NOW)
+        );
     this->date = date;
 };
 
-Transaction::GetDate(){ return this->Date; };
-
-Transaction::Transaction(int hour, double amount){
-
-};
-
-
-
-#endif
+Date Transaction::GetDate()
+    { return this->date; };
--- tests/src/transaction.hpp
+++ tests/src/transaction.hpp
@@ -1,12 +1,17 @@
-#include <time.h>
 
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include <time.h>
 enum TRANSACTION_TYPE { BUY_STOCK, SELL_STOCK, DEPOSIT, WITHDRAW };
 enum DAYS_OF_MONTH { JAN = 31, FEB = 28, MAR = 31, APR = 30, MAY = 31, JUN = 30,
             JUL = 31, AUG = 31, SEP = 30, OCT = 31, NOV = 30, DEC = 31 };
-enum MONTH = { JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
-struct Date {
+//enum MONTH { JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
+class Date {
+public:
+    //Date(int, int, int, int, string);
+    Date();
     Date(int, int, int, int, string);
-
     int year;
     int month;
     int day;
@@ -19,9 +24,10 @@ struct Date {
 class Transaction {
 public:
     // Date | Status | Type | Amount/Postion | Status
+
     // Constructor
     Transaction();
-    Transaction(int hour, double amount);
+    //Transaction(int hour, double amount);
 
     // Get
     Date GetDate();
@@ -34,3 +40,5 @@ private:
     int type;
     double amount;
 };
+
+#endif
