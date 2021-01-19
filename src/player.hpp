1b12eb7340aeeebd441d6eadb05881c87de1b547
katsuyaw <kw@kmail.me>
2021-01-18 20:15:24 -0800

    Change  bankAccount to a pointer

--- src/player.hpp
+++ src/player.hpp
@@ -6,7 +6,7 @@ using namespace std;
 class Player{
     private:
         string playerName;
-        Account bankAccount;
+        Account* bankAccount;
         vector< pair <string, int> >;
         
         
