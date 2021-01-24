0f5cca135685112771098a4d8f30813dfeba8026
katsuyaw <kw@kmail.me>
2021-01-24 00:11:31 -0800

    Add human setters and getters

--- src/human.cpp
+++ src/human.cpp
@@ -8,8 +8,11 @@ Human(string fN, int a){
 private:
     this->firstName = fN;
     this->age = a;
+    
 }
-void Human::setAge(int a){this->age = a;};
+void Human::setAge(int a){ this->age = a; };
 int  Human::getAge(){ return this->age; };
-void Human::setFirstName(string fN){this->firstName = fN;};
+void Human::setFirstName(string fN){ this->firstName = fN; };
 int  Human::getFirstName(){ return this->age; };
+void Human::setUniqueID(auto u){ this->uniqueID = u };
+string Human::getUniqueID(){ this->uniqueID; }
