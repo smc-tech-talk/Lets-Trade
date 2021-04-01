f3a3792db68d79328653217df7b3492a014792f1
Bundy-Mundi <bennie.ben095@gmail.com>
2021-04-01 16:49:52 +0900

    Fixed banking.cpp & small errors

--- src/banking.hpp
+++ src/banking.hpp
@@ -9,20 +9,6 @@
 using namespace std;
 
 
-string list_bank[9]{
-	"CIT Bank", "Bank of The West", "WELLS FARGO",
-	"Washington Federal Savings and Loan", "Umpqua Bank",
-	"Golden1 Credit Union", "EASTWEST BANK", "Charles Schwab", "CHASE"
-};
-
-char codebook[] = { 'a','A','b','B','c','C','d','D','e',
-					'E','f','F','g','G','h','H','i','I','j','J','k','K',
-					'l','L','m','M','n','N','o','O','p','P','q','Q','r',
-					'R','s','S','t','T','u','U','v','V','w','W','x','X',
-					'y','Y','z','Z','0','1','2','3','4','5','6','7','8','9',
-					'!','@','#','$','%','&','*' };
-
-
 class Account {
 
 public:
@@ -36,7 +22,7 @@ public:
 	// Account(Player* p, string accountName, Bank* b); => A constructor using 'Bank' class
 	//~Account();
 
-	
+
 	vector <string> log;
 
 
@@ -48,7 +34,7 @@ public:
 	void print_record();
 	void input_record(Transaction t, Date* d);
 	void info_Account();
-	
+
 
 private:
 	Player* player;
